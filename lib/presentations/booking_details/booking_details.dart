d75e61f62d23fff6b231bf9da8aa842fb115cdcf
Tarek Elsayed <Tarekelsayed44>
2023-04-10 01:03:24 +0200

    image_picker

---
+++ lib/presentations/booking_details/booking_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class booking_details extends StatefulWidget {
+  const booking_details({Key? key}) : super(key: key);
+
+  @override
+  State<booking_details> createState() => _booking_detailsState();
+}
+
+class _booking_detailsState extends State<booking_details> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
--- lib/presentations/sign_up/sign_up1.dart
+++ lib/presentations/sign_up/sign_up1.dart
@@ -1,6 +1,12 @@
+
+
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:pick_park/presentations/resources/assets_manager.dart';
 import 'package:pick_park/shared/components/component.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../domain/auth/auth_cubit.dart';
@@ -17,12 +23,14 @@ class Register_form extends StatefulWidget {
 }
 
 class _Register_formState extends State<Register_form> {
+   late PickedFile _imageFile;
   var nameController = TextEditingController();
   var birthController = TextEditingController();
   var emailController = TextEditingController();
   var phoneController = TextEditingController();
   var formKey = GlobalKey<FormState>();
   var selctedGender;
+  final ImagePicker _picker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit,AuthStates>(
@@ -90,12 +98,17 @@ class _Register_formState extends State<Register_form> {
                       CircleAvatar(
                         radius: 70,
                         backgroundColor: Colors.grey,
+                        backgroundImage: _imageFile == null? null:FileImage(File(_imageFile.path)),
                       ),
                       Positioned(
                         bottom: 20.0,
                         right: 20.0,
                         child: InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            showModalBottomSheet(
+                                context: context,
+                                builder: ((builder) => bottomSheet()));
+                          },
                           child: Icon(
                             Icons.camera_alt,
                             color: Colors.teal,
@@ -207,4 +220,42 @@ class _Register_formState extends State<Register_form> {
 
     );
   }
+   Widget bottomSheet() {
+     return Container(
+       height: 100.0,
+       width: MediaQuery.of(context).size.width,
+       margin: EdgeInsets.symmetric(
+         horizontal: 20,
+         vertical: 20,
+       ),
+       child: Column(
+         children: <Widget>[
+           Text(
+             "Choose Profile photo",
+             style: TextStyle(
+               fontSize: 20.0,
+             ),
+           ),
+           SizedBox(
+             height: 20,
+           ),
+           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+             FloatingActionButton(onPressed: (){ takePhoto(ImageSource.camera);},
+               child: Icon(Icons.camera_alt),
+             ),
+            FloatingActionButton(onPressed: (){takePhoto(ImageSource.gallery);},child: Icon(Icons.photo),)
+           ])
+         ],
+       ),
+     );
+   }
+
+   void takePhoto(ImageSource source) async {
+     final pickedFile = await _picker.getImage(
+       source: source,
+     );
+     setState(() {
+       _imageFile = pickedFile!;
+     });
+   }
 }
