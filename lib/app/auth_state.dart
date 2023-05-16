0138ed1fe5bfb8c1b374de3e90973843ac2e578b
Tarek Elsayed <Tarekelsayed44>
2023-05-16 15:05:57 +0300

    Authentication

---
+++ lib/app/auth_state.dart
@@ -0,0 +1,19 @@
+part of 'bloc_observer.dart';
+abstract class AuthStates{}
+class AuthInitialState extends AuthStates{}
+
+class RegisterLoadingState extends AuthStates{}
+class RegisterSuccessState extends AuthStates{}
+class RegisterFailedState extends AuthStates{
+  String message;
+  RegisterFailedState({required this.message});
+
+}
+
+class loginLoadingState extends AuthStates{}
+class loginSuccessState extends AuthStates{}
+class loginFailedState extends AuthStates{
+  String message;
+  loginFailedState({required this.message});
+}
+
